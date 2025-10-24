37e8f838cee6e9f520c0834500c895fdf079510a
ovi <eu_oni@yahoo.com>
2025-10-24 22:59:13 +0300

    starting exe 1

---
+++ 01-algorithmics/01-sequential-flow/main.js
@@ -0,0 +1,11 @@
+// prompt('Enter your number');
+// let yourNumer=prompt('Enter your number');
+// yourNumer=Number(yourNumer);
+let yourNumer=Number(prompt('Enter your number'));
+
+let opposite=yourNumer*-1;
+let remainer=yourNumer%2;
+let thePower=2**yourNumer;
+console.log(`the opposide is ${opposite}, the remainer is ${remainer}, the power of 2 is ${thePower}`);
+console.log(remainer);
+console.log(thePower);
